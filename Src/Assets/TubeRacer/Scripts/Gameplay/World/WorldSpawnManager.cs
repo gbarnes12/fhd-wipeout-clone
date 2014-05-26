47df94784e86f9d17682c7a8015f0ffd3abd5d90
gavin.barnes90 <gavin.barnes90@googlemail.com>
2014-05-26 19:56:11 +0000

    - added basic folder structure - added image effects (this is pro only!) - added basic algorithm for tube generation (world generator) - added event system - added some basic project settings - added world loader! This needs adjustments as well since it's suited for DaikonForge UI system at the moment!

---
+++ Src/Assets/TubeRacer/Scripts/Gameplay/World/WorldSpawnManager.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using System.Collections;
+using Utilities;
+using System.Collections.Generic;
+
+
+namespace Gameplay.World
+{
+    /// <summary>
+    /// The class is used in order to move the world and spawn new items,
+	/// thus the racing track seems to be infinite. This is achieved by 
+	/// replacing the tubes at the end of the track once we passed them!
+    /// </summary>
+    public class WorldSpawnManager : Singleton<WorldSpawnManager>
+    {
+        #region Inner Class
+        /// <summary>
+        /// 
+        /// </summary>
+        private class WorldChunkSorter : IComparer<WorldTubeChunk>
+        {
+            /// <summary>
+            /// Compares two world tube chunk elements by it's z component!
+            /// </summary>
+            /// <param name="x"></param>
+            /// <param name="y"></param>
+            /// <returns></returns>
+            public int Compare(WorldTubeChunk x, WorldTubeChunk y)
+            {
+                return x.transform.position.z.CompareTo(y.transform.position.z);
+            }
+        }
+        #endregion
+
+        #region Private Members
+        private LinkedList<WorldTubeChunk> _tubeChunksList;
+        private List<WorldTubeChunk> _tubeChunksQueue;
+        #endregion
+
+        #region Construct
+        /// <summary>
+        /// Protected in order to prevent copying!
+        /// </summary>
+        protected WorldSpawnManager() 
+        {
+            this._tubeChunksQueue = new List<WorldTubeChunk>();
+        }
+        #endregion
+
+        #region Unity Methods
+        /// <summary>
+        /// Initializes the world.
+        /// </summary>
+        void Start()
+        {
+            GameObject levelObject = GameObject.FindGameObjectWithTag("World");
+            if (levelObject == null)
+            {
+                Debug.LogError("There is no world object in the scene please add one!");
+                return;
+            }
+
+            WorldTubeChunk[] chunks = levelObject.GetComponentsInChildren<WorldTubeChunk>();
+            if (chunks.Length == 0)
+            {
+                Debug.LogError("There are no world tube chunk elements!");
+                return;
+            }
+
+            List<WorldTubeChunk> temporaryChunkList = new List<WorldTubeChunk>();
+            foreach (WorldTubeChunk chunk in chunks)
+                temporaryChunkList.Add(chunk);
+
+            temporaryChunkList.Sort(new WorldChunkSorter());
+            this._tubeChunksList = new LinkedList<WorldTubeChunk>(temporaryChunkList);
+        }
+
+        /// <summary>
+        /// Moves the tubes!
+        /// </summary>
+        void Update()
+        {
+            foreach(WorldTubeChunk tube in this._tubeChunksList)
+            {
+                tube.transform.Translate(new Vector3(0.0f, 0.0f, -1.0f * Time.deltaTime * 50.0f));
+            }
+        }
+        #endregion
+
+        #region Private Methods
+        /// <summary>
+        /// Spawns the queued chunks after we have collected three! This is used as a coroutine!
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerator SpawnTubeChunks()
+        {
+            List<WorldTubeChunk> copiedList = new List<WorldTubeChunk>(this._tubeChunksQueue);
+            
+            foreach(WorldTubeChunk chunk in copiedList)
+            {
+                yield return null;
+
+                LinkedListNode<WorldTubeChunk> lastNode = this._tubeChunksList.Last;
+                chunk.transform.position = new Vector3(chunk.transform.position.x, chunk.transform.position.y, lastNode.Value.transform.position.z + 30);
+                this._tubeChunksList.AddLast(chunk);
+                this._tubeChunksQueue.Remove(chunk);
+				chunk.gameObject.SetActive(true);
+            }
+
+            copiedList.Clear();
+            yield break;
+        }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Adds the chunk to the queue in order to respawn it.
+        /// </summary>
+        /// <param name="chunk">The chunk</param>
+        public void AddChunkToQueue(WorldTubeChunk chunk)
+        {
+			if (chunk == null)
+				return;
+
+            this._tubeChunksList.Remove(chunk);
+            this._tubeChunksQueue.Add(chunk);
+			chunk.gameObject.SetActive(false);
+
+             if(this._tubeChunksQueue.Count >= 3)
+                 StartCoroutine("SpawnTubeChunks");
+        }
+        #endregion
+
+    }
+}
---
+++ Src/Assets/TubeRacer/Scripts/Utilities/Loading/WorldLoadingManager.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Utilities;
+using Utilities.Loading.Processors;
+
+namespace Utilities.Loading
+{
+    #region Event Types
+	/// <summary>
+	/// World loading finished event.
+	/// </summary>
+    public class WorldLoadingFinishedEvent {}
+	/// <summary>
+	/// World loading failed event.
+	/// </summary>
+    public class WorldLoadingFailedEvent
+    {
+        public string Reason;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Utilities.Loading.WorldLoadingFailedEvent"/> class.
+		/// </summary>
+		/// <param name="reason">Reason.</param>
+        public WorldLoadingFailedEvent(string reason)
+        {
+            this.Reason = reason;
+        }
+    }
+    #endregion
+
+    /// <summary>
+    /// The world loading manager can be used to load 
+    /// a new level with the necessary steps!
+    /// </summary>
+    public class WorldLoadingManager : Singleton<WorldLoadingManager>
+    {
+        #region Private Members
+        private List<WorldLoadingProcessor> _processors;
+        private WorldLoadingProcessor _activeLoader;
+        private WorldLoadingProcessor _previousLoader;
+        private bool _isLoading;
+        private string _sceneFile;
+        private int _i;
+        #endregion
+
+		#region Constructor
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Utilities.Loading.WorldLoadingManager"/> class.
+		/// </summary>
+		protected WorldLoadingManager() 
+        {
+            this._processors = new List<WorldLoadingProcessor>();
+            this._isLoading = false;
+            this._activeLoader = null;
+            this._previousLoader = null;
+            this._sceneFile = null;
+        }
+        #endregion
+
+        #region Unity Methods
+		/// <summary>
+		/// Update this instance.
+		/// </summary>
+        void Update()
+        {
+            if (!this._isLoading)
+                return;
+
+            if (this._sceneFile == null)
+                return;
+
+            if (this._processors.Count == 0)
+                return;
+
+            if (!this._activeLoader.Run(this._previousLoader))
+                return;
+
+            // increment the counter!
+            this._i++;
+
+            if(this._processors.Count == this._i)
+            {
+                this._processors.Clear();
+                this._isLoading = false;
+                this._i = 0;
+                this._previousLoader = null;
+                this._activeLoader = null;
+
+				GameObject guiManager = GameObject.Find ("Loading UI Root");
+                if (guiManager == null) 
+                {
+                    EventSystem.EventSystem.getInstance().executeEvent<WorldLoadingFailedEvent>(
+                                               new WorldLoadingFailedEvent("No Loading Screen UI root found!"),
+                                               EventSystem.EventSystem.ExecuteMode.Immediate);
+                }
+				
+                GameObject.Destroy(guiManager);
+
+                EventSystem.EventSystem.getInstance().executeEvent<WorldLoadingFinishedEvent>(
+                                new WorldLoadingFinishedEvent(), 
+                                EventSystem.EventSystem.ExecuteMode.Immediate);
+
+                return;
+            }
+
+            this._previousLoader = this._activeLoader;
+            this._activeLoader = this._processors[this._i];
+            this._activeLoader.Start(this._sceneFile);
+
+        }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Loads a new level with all the necessary networking steps!
+        /// </summary>
+        /// <param name="name"></param>
+        public void LoadLevel(string name)
+        {
+            if (this._isLoading)
+            {
+                EventSystem.EventSystem.getInstance().executeEvent<WorldLoadingFailedEvent>(
+                                                new WorldLoadingFailedEvent("Manager is already loading!"), 
+                                                EventSystem.EventSystem.ExecuteMode.Immediate);
+                return;
+            }
+            
+            this._processors.Clear();
+            this._i = 0;
+ 
+            // load the loading scene!
+            Application.LoadLevel("Scn_Glbl_Loading");
+
+            this._sceneFile = name;
+            this._isLoading = true;
+
+            // create processors
+            this._processors.Add(new SceneLoadingProcessor());
+
+            // if there is no active loader yet
+            this._activeLoader = _processors[this._i];
+            this._activeLoader.Start(this._sceneFile);
+        }
+        #endregion
+    }
+}
