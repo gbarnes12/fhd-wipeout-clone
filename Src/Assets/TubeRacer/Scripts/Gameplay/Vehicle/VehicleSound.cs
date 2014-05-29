3dc9be7bdaee3abff2d78e02dc65ed16289e6c6b
gavin.barnes90 <gavin.barnes90@googlemail.com>
2014-05-29 14:39:41 +0000

    - moved some of the files to different folders and renamed them (file naming conventions!) - re factored some scripts thus they fit with the coding standards

---
+++ Src/Assets/TubeRacer/Scripts/Gameplay/Vehicle/VehicleSound.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Gameplay.Vehicle 
+{
+	public class VehicleSound : MonoBehaviour 
+	{
+		//------------------
+		//FINAL
+
+		#region Public Inspector Members
+		public float MediaSpeed = 100.0f; //speed at which the audio pitch is 1.0
+		#endregion
+
+		#region Private Members
+		private float _currentSpeed = 100.0f;
+		#endregion
+
+		#region Unity Methods
+		void Update () 
+		{
+			EngineSound ();
+		}
+
+		void EngineSound() 
+		{
+			//TO-DO: Get Speed of Current Tube
+			//currentSpeed = getCurrentSpeed();
+			audio.pitch = this._currentSpeed / this.MediaSpeed;
+		}
+		#endregion
+	}
+}
