5ec6c34234641edca525854df78cfe407efb9d5e
a.delas485@gmail.com <a.delas485@gmail.com@88e5773d-5d6b-f470-fd96-34c002b47934>
2014-05-31 15:04:48 +0000

    added gameobjects for engine and evade sound. evade sound plays when pressing the horizontal keys.

--- trunk/Src/Assets/TubeRacer/Scripts/Gameplay/Vehicle/VehicleSound.cs
+++ trunk/Src/Assets/TubeRacer/Scripts/Gameplay/Vehicle/VehicleSound.cs
@@ -14,6 +14,8 @@ namespace Gameplay.Vehicle
 
 		#region Public Inspector Members
 		public float MediaSpeed = 100.0f; //speed at which the audio pitch is 1.0
+		public AudioSource vehicleEngine;
+		public AudioSource vehicleEvade;
 		#endregion
 
 		#region Private Members
@@ -27,6 +29,8 @@ namespace Gameplay.Vehicle
 		void Update () 
 		{
 			EngineSound ();
+			EvadeSound ();
+
 		}
 
 		/// <summary>
@@ -36,7 +40,23 @@ namespace Gameplay.Vehicle
 		{
 			//TO-DO: Get Speed of Current Tube
 			//currentSpeed = getCurrentSpeed();
-			audio.pitch = this._currentSpeed / this.MediaSpeed;
+			vehicleEngine.pitch = this._currentSpeed / this.MediaSpeed;
+		}
+
+		/// <summary>
+		/// Plays the evade sound.
+		/// </summary>
+		void EvadeSound() 
+		{
+			if (Input.GetButtonDown ("Horizontal")) {
+				vehicleEvade.loop = true;
+				vehicleEvade.Play ();
+				
+			}
+			if (Input.GetButtonUp("Horizontal")) {
+				vehicleEvade.loop = false;
+				vehicleEvade.Stop();
+			}
 		}
 		#endregion
 	}
