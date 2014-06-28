aade1f70ba7edfac823022f43152a9aabb42971e
a.delas485 <a.delas485@gmail.com>
2014-06-28 23:06:26 +0000

    speed now updating per frame; motor sound pitch now based on actual speed: maximum speed for vehicle = 1200. minimum speed = 100. actual vehicle/world speed not influenced by these numbers.

--- Src/Assets/TubeRacer/Scripts/Gameplay/World/WorldSpawnManager.cs
+++ Src/Assets/TubeRacer/Scripts/Gameplay/World/WorldSpawnManager.cs
@@ -6,129 +6,135 @@ using System.Collections.Generic;
 
 namespace Gameplay.World
 {
-    /// <summary>
-    /// The class is used in order to move the world and spawn new items,
-	/// thus the racing track seems to be infinite. This is achieved by 
+	/// <summary>
+	/// The class is used in order to move the world and spawn new items,
+	/// thus the racing track seems to be infinite. This is achieved by
 	/// replacing the tubes at the end of the track once we passed them!
-    /// </summary>
-    public class WorldSpawnManager : Singleton<WorldSpawnManager>
-    {
-        #region Inner Class
-        /// <summary>
-        /// 
-        /// </summary>
-        private class WorldChunkSorter : IComparer<WorldTubeChunk>
-        {
-            /// <summary>
-            /// Compares two world tube chunk elements by it's z component!
-            /// </summary>
-            /// <param name="x"></param>
-            /// <param name="y"></param>
-            /// <returns></returns>
-            public int Compare(WorldTubeChunk x, WorldTubeChunk y)
-            {
-                return x.transform.position.z.CompareTo(y.transform.position.z);
-            }
-        }
-        #endregion
-
-        #region Private Members
-        private LinkedList<WorldTubeChunk> _tubeChunksList;
-        private List<WorldTubeChunk> _tubeChunksQueue;
+	/// </summary>
+	public class WorldSpawnManager : Singleton<WorldSpawnManager>
+	{
+		#region Inner Class
+		/// <summary>
+		///
+		/// </summary>
+		private class WorldChunkSorter : IComparer<WorldTubeChunk>
+		{
+			/// <summary>
+			/// Compares two world tube chunk elements by it's z component!
+			/// </summary>
+			/// <param name="x"></param>
+			/// <param name="y"></param>
+			/// <returns></returns>
+			public int Compare(WorldTubeChunk x, WorldTubeChunk y)
+			{
+				return x.transform.position.z.CompareTo(y.transform.position.z);
+			}
+		}
+		#endregion
+		
+		#region Private Members
+		private LinkedList<WorldTubeChunk> _tubeChunksList;
+		private List<WorldTubeChunk> _tubeChunksQueue;
 		private List<WorldObstacle> _obstaclesList;
 		private float _vehicleSpeed;
-        #endregion
-
-		#region Public Members
-
 		#endregion
-
-        #region Construct
-        /// <summary>
-        /// Protected in order to prevent copying!
-        /// </summary>
-        protected WorldSpawnManager() 
-        {
-            this._tubeChunksQueue = new List<WorldTubeChunk>();
+		
+		#region Public Members
+		
+		VehicleController controller;
+		
+		#endregion
+		
+		#region Construct
+		/// <summary>
+		/// Protected in order to prevent copying!
+		/// </summary>
+		protected WorldSpawnManager()
+		{
+			this._tubeChunksQueue = new List<WorldTubeChunk>();
 			this._vehicleSpeed = 400.0f;
-        }
-        #endregion
-
-        #region Unity Methods
-        /// <summary>
-        /// Initializes the world.
-        /// </summary>
-        void Start()
-        {
+		}
+		#endregion
+		
+		#region Unity Methods
+		/// <summary>
+		/// Initializes the world.
+		/// </summary>
+		void Start()
+		{
 			GameObject player = GameObject.FindGameObjectWithTag ("Player");
-			if (player == null) 
+			if (player == null)
 			{
 				Debug.LogError("There is no player object in the scene!");
 				return;
 			}
-
-			VehicleController controller = player.GetComponent<VehicleController> ();
+			
+			controller  = player.GetComponent<VehicleController> ();
 			this._vehicleSpeed = controller.Speed;
-
-            GameObject levelObject = GameObject.FindGameObjectWithTag("World");
-            if (levelObject == null)
-            {
-                Debug.LogError("There is no world object in the scene please add one!");
-                return;
-            }
-
-            WorldTubeChunk[] chunks = levelObject.GetComponentsInChildren<WorldTubeChunk>();
-            if (chunks.Length == 0)
-            {
-                Debug.LogError("There are no world tube chunk elements!");
-                return;
-            }
-
-            List<WorldTubeChunk> temporaryChunkList = new List<WorldTubeChunk>();
-            foreach (WorldTubeChunk chunk in chunks)
-                temporaryChunkList.Add(chunk);
-
-            temporaryChunkList.Sort(new WorldChunkSorter());
-            this._tubeChunksList = new LinkedList<WorldTubeChunk>(temporaryChunkList);
-        }
-
-        /// <summary>
-        /// Moves the tubes!
-        /// </summary>
-        void Update()
-        {
-            foreach(WorldTubeChunk tube in this._tubeChunksList)
-            {
+			
+			GameObject levelObject = GameObject.FindGameObjectWithTag("World");
+			if (levelObject == null)
+			{
+				Debug.LogError("There is no world object in the scene please add one!");
+				return;
+			}
+			
+			WorldTubeChunk[] chunks = levelObject.GetComponentsInChildren<WorldTubeChunk>();
+			if (chunks.Length == 0)
+			{
+				Debug.LogError("There are no world tube chunk elements!");
+				return;
+			}
+			
+			List<WorldTubeChunk> temporaryChunkList = new List<WorldTubeChunk>();
+			foreach (WorldTubeChunk chunk in chunks)
+				temporaryChunkList.Add(chunk);
+			
+			temporaryChunkList.Sort(new WorldChunkSorter());
+			this._tubeChunksList = new LinkedList<WorldTubeChunk>(temporaryChunkList);
+		}
+		
+		/// <summary>
+		/// Moves the tubes!
+		/// </summary>
+		void Update()
+		{
+			this._vehicleSpeed = controller.Speed;
+			foreach(WorldTubeChunk tube in this._tubeChunksList)
+			{
 				tube.transform.Translate(new Vector3(0.0f, 0.0f, -1.0f * Time.deltaTime * this._vehicleSpeed));
-            }
-        }
-        #endregion
+			}
+			
 
-        #region Private Methods
-        /// <summary>
-        /// Spawns the queued chunks after we have collected three! This is used as a coroutine!
-        /// </summary>
-        /// <returns></returns>
-        private IEnumerator SpawnTubeChunks()
-        {
-            List<WorldTubeChunk> copiedList = new List<WorldTubeChunk>(this._tubeChunksQueue);
-
-            foreach(WorldTubeChunk chunk in copiedList)
-            {
-                yield return null;
-
-                LinkedListNode<WorldTubeChunk> lastNode = this._tubeChunksList.Last;
-                chunk.transform.position = new Vector3(chunk.transform.position.x, chunk.transform.position.y, lastNode.Value.transform.position.z + 350);
-                this._tubeChunksList.AddLast(chunk);
-                this._tubeChunksQueue.Remove(chunk);
+			
+		}
+		#endregion
+		
+		#region Private Methods
+		/// <summary>
+		/// Spawns the queued chunks after we have collected three! This is used as a coroutine!
+		/// </summary>
+		/// <returns></returns>
+		private IEnumerator SpawnTubeChunks()
+		{
+			List<WorldTubeChunk> copiedList = new List<WorldTubeChunk>(this._tubeChunksQueue);
+			
+			foreach(WorldTubeChunk chunk in copiedList)
+			{
+				yield return null;
+				
+				LinkedListNode<WorldTubeChunk> lastNode = this._tubeChunksList.Last;
+				chunk.transform.position = new Vector3(chunk.transform.position.x, chunk.transform.position.y, lastNode.Value.transform.position.z + 350);
+				this._tubeChunksList.AddLast(chunk);
+				this._tubeChunksQueue.Remove(chunk);
 				AddRandomObstacleToChunk(chunk);
 				chunk.gameObject.SetActive(true);
-            }
-
-            copiedList.Clear();
-            yield break;
-        }
-
+			}
+			
+			copiedList.Clear();
+			yield break;
+		}
+		
 		/// <summary>
 		/// Adds the random obstacle.
 		/// </summary>
@@ -137,17 +143,17 @@ namespace Gameplay.World
 		{
 			if(_obstaclesList == null)
 				return;
-
+			
 			int rndObstacle = (int)Random.Range(0, _obstaclesList.Count);
 			WorldObstacle obstacle = (rndObstacle > _obstaclesList.Count)? _obstaclesList[rndObstacle-1] : _obstaclesList[rndObstacle];
-
+			
 			if(obstacle.gameObject.activeSelf)
 				return;
-
+			
 			obstacle.AttachToChunk(chunk);
 			obstacle.gameObject.SetActive(true);
 		}
-
+		
 		/// <summary>
 		/// Removes the obstacles from chunk.
 		/// </summary>
@@ -162,29 +168,29 @@ namespace Gameplay.World
 				yield return null;
 			}
 		}
-        #endregion
-
-        #region Public Methods
-        /// <summary>
-        /// Adds the chunk to the queue in order to respawn it.
-        /// </summary>
-        /// <param name="chunk">The chunk</param>
-        public IEnumerator AddChunkToQueue(WorldTubeChunk chunk)
-        {
+		#endregion
+		
+		#region Public Methods
+		/// <summary>
+		/// Adds the chunk to the queue in order to respawn it.
+		/// </summary>
+		/// <param name="chunk">The chunk</param>
+		public IEnumerator AddChunkToQueue(WorldTubeChunk chunk)
+		{
 			if (chunk == null)
 				yield break;
-
-            this._tubeChunksList.Remove(chunk);
-            this._tubeChunksQueue.Add(chunk);
-
+			
+			this._tubeChunksList.Remove(chunk);
+			this._tubeChunksQueue.Add(chunk);
+			
 			StartCoroutine(RemoveObstaclesFromChunk(chunk));
-
+			
 			chunk.gameObject.SetActive(false);
-
-             if(this._tubeChunksQueue.Count >= 3)
-                 StartCoroutine("SpawnTubeChunks");
-        }
-
+			
+			if(this._tubeChunksQueue.Count >= 3)
+				StartCoroutine("SpawnTubeChunks");
+		}
+		
 		/// <summary>
 		/// Registers the obstacle.
 		/// </summary>
@@ -193,13 +199,13 @@ namespace Gameplay.World
 		{
 			if(_obstaclesList == null)
 				_obstaclesList = new List<WorldObstacle>();
-
+			
 			if(obstacle == null)
 				return;
-
+			
 			this._obstaclesList.Add (obstacle);
 		}
-        #endregion
-
-    }
+		#endregion
+		
+	}
 }
