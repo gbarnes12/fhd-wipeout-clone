5c651d944cb79f28029239070f2adb3f90221966
a.delas485@gmail.com <a.delas485@gmail.com@88e5773d-5d6b-f470-fd96-34c002b47934>
2014-07-10 11:38:58 +0000

    Added ScoreScript to WorldManager (Please run newGame() or set gameRunning to false when new game has started/ended)

---
+++ trunk/Src/Assets/TubeRacer/Scripts/Gameplay/Score/ScoreController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreController : MonoBehaviour {
+
+
+
+	//Wenn die Runde ENDET wird gameRunning auf false gesetzt
+	public bool gameRunning;
+
+
+
+	//Wenn die Runde STARTET wird newGame() aufgerufen
+	public void newGame() {
+		gameRunning = true;
+		currentFrame = 0;
+		points = 0;
+	}
+
+
+
+
+	private int currentFrame = 0;
+	private int points = 0;
+
+	void Start() {
+		newGame ();
+	}
+
+	void Update () {
+		if (gameRunning) {
+			currentFrame++;
+			CountPoints();
+		}
+	}
+
+	private void CountPoints() {
+		if (currentFrame % 10 == 0) {
+			points++;
+			Debug.Log (points);
+		}
+
+	}
+
+	void OnGUI() {
+			GUI.Box (new Rect (10, 10, 50, 40), "Points" + "\n" + points);
+
+	}
+
+
+
+
+}
