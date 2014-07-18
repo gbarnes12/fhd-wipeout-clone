28d72dcd09859c07b5317a633cf93b19a06a25c6
anmi.nguyen@gmail.com <anmi.nguyen@gmail.com@88e5773d-5d6b-f470-fd96-34c002b47934>
2014-07-18 15:57:55 +0000

    - Added different obstacles behavior - Added keyboard input for testing without kinect - Cleaned up part of assets - Refactored scripts - Updated tube and obstacles prefab with correct models

--- trunk/Src/Assets/TubeRacer/Scripts/Gameplay/Vehicle/VehicleCollision.cs
+++ trunk/Src/Assets/TubeRacer/Scripts/Gameplay/Vehicle/VehicleCollision.cs
@@ -1,58 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
-using Gameplay.Vehicle;
+using Gameplay.World;
 
-public class VehicleCollision : MonoBehaviour 
+namespace Gameplay.Vehicle
 {
+	public class VehicleCollision : MonoBehaviour 
+	{
+		#region Public Inspector Members
+		public WorldSpawnManager worldSpawnManager;
 
-	public Transform explosion;
-	public Transform fire;
-	public AudioSource vehicleExplosion;
+		public Transform explosion;
+		public Transform fire;
+		public AudioSource vehicleExplosion;
+		#endregion
 
-	private VehicleController vehicleController;
-	private VehicleSound vehicleSound;
-	private ScoreController scoreController;
-	private GuiReplay guiReplay;
+		#region Private Members
+		private VehicleController vehicleController;
+		private VehicleSound vehicleSound;
+		private ScoreController scoreController;
+		private GuiReplay guiReplay;
 
-	private GameObject player;
-	private GameObject vehicle;
-	private GameObject thruster;
+		private GameObject player;
+		private GameObject vehicle;
+		private GameObject thruster;
+		#endregion
 
+		#region Unity Methods
+		/// <summary>
+		/// Start this instance.
+		/// </summary>
+		void Start(){
+			guiReplay = GameObject.FindGameObjectWithTag ("GUI_Replay").GetComponent<GuiReplay>(); 
+			worldSpawnManager = GameObject.FindGameObjectWithTag ("WorldManager").GetComponent<WorldSpawnManager> ();
+			scoreController = worldSpawnManager.gameObject.GetComponent<ScoreController>();
+			thruster = GameObject.FindGameObjectWithTag ("Thruster");
+			player = GameObject.FindGameObjectWithTag ("Player");
 
-	void Start(){
-		guiReplay = GameObject.FindGameObjectWithTag ("GUI_Replay").GetComponent<GuiReplay>(); 
-		scoreController = GameObject.FindGameObjectWithTag ("WorldManager").GetComponent<ScoreController> ();
-		thruster = GameObject.FindGameObjectWithTag ("Thruster");
-		player = GameObject.FindGameObjectWithTag ("Player");
+			vehicleSound = player.GetComponent<VehicleSound> ();
+			vehicleController = player.GetComponent<VehicleController>();
 
-		vehicleSound = player.GetComponent<VehicleSound> ();
-		vehicleController = player.GetComponent<VehicleController>();
-
-		vehicleSound.GameRunning = true;
-
-	}
-
-	void OnCollisionEnter(Collision other){
-
-		if (!other.gameObject.name.Equals("Prfb_Lvl_Tube")) {
-
-				vehicleExplosion.Play();
-
-				vehicleSound.GameRunning = false;
-				scoreController.gameRunning = false;
-				vehicleController.GameRunning = false;
-
-				
-				guiReplay.StartMenuReplay();
-
-				//Vector3 posRacer = gameObject.transform.position+new Vector3(0,0,-13.5f);
-
-				//Object exp = Instantiate(explosion, posRacer, Quaternion.identity);
-				//Object flames = Instantiate(fire, posRacer, Quaternion.identity);
-
-				Debug.Log ("Collision bla with " + other.gameObject.name);
+			vehicleSound.GameRunning = true;
 
 		}
+
+		/// <summary>
+		/// Raises the collision enter event.
+		/// </summary>
+		/// <param name="other">Other.</param>
+		void OnCollisionEnter(Collision other){
+
+			if (other.gameObject.tag == "Obstacle") 
+			{
+					vehicleExplosion.Play();
+
+					worldSpawnManager.GameRunning = false;
+					vehicleSound.GameRunning = false;
+					scoreController.gameRunning = false;
+					vehicleController.GameRunning = false;
+
+					
+					guiReplay.StartMenuReplay();
+
+					//Vector3 posRacer = gameObject.transform.position+new Vector3(0,0,-13.5f);
+
+					//Object exp = Instantiate(explosion, posRacer, Quaternion.identity);
+					//Object flames = Instantiate(fire, posRacer, Quaternion.identity);
+
+					Debug.Log ("Collision bla with " + other.gameObject.name);
+			}
+		}
+		#endregion
 	}
-	
 }
