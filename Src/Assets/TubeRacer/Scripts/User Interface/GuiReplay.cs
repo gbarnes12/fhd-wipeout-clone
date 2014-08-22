5ec3450c440862998236ce0e45ae36bf1c67a0b8
anmi.nguyen <anmi.nguyen@gmail.com>
2014-08-22 12:31:54 +0000

    - Added kinect to main scene - Tweaked visuals (ambient lights, material colors) - Implemented stereoscopic  GUI - Tweaked racer speed, rotation velocity, inverted kinect control direction

--- Src/Assets/TubeRacer/Scripts/User Interface/GuiReplay.cs
+++ Src/Assets/TubeRacer/Scripts/User Interface/GuiReplay.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 public class GuiReplay : MonoBehaviour {
-
+	
 	#region Private Members
 	private Texture2D _texMenuBackground;
 	private Texture2D _texMenuButton;
@@ -10,24 +10,25 @@ public class GuiReplay : MonoBehaviour {
 	private Texture2D _texMenuTextfield;
 	private Texture2D _texMenuBackground2;
 	private Font _font;
-
+	
 	private float _positionGuiX = 0.0f;
-
+	
 	private GuiHighscoreMenu _highscore;
 	private GuiCreditsMenu _credits;
 	private float _menuWidth;
 	private float _speed = 800.0f;
-
+	
 	private string _playerName = "";
 	private bool _isNewHighscore = false;
 	#endregion
-
+	
 	#region Public Members
 	public bool _showMenuReplay = false;
+	public bool Enable3D;
 	public string _gameScene = "Scn_Wrld_Game";
 	public string _menuScene = "Scn_Gui_MainMenu";
 	#endregion
-
+	
 	#region Unity Methods
 	/// <summary>
 	/// Awake this instance.
@@ -35,11 +36,11 @@ public class GuiReplay : MonoBehaviour {
 	void Awake () 
 	{
 		//StartMenuReplay ();
-	
+		
 		//Andere Menu teile laden
 		_highscore = this.GetComponent <GuiHighscoreMenu> ();
 		_credits = this.GetComponent <GuiCreditsMenu> ();
-
+		
 		//Texturen und Fonts laden
 		_texMenuBackground = Resources.Load<Texture2D> ("GUI/Textures/TexGuiBackground");
 		_texMenuButton = Resources.Load<Texture2D> ("GUI/Textures/TexGuiButton");
@@ -47,8 +48,8 @@ public class GuiReplay : MonoBehaviour {
 		_texMenuTextfield = Resources.Load<Texture2D> ("GUI/Textures/TexGuiTextfield");
 		_texMenuBackground2 = Resources.Load<Texture2D> ("GUI/Textures/TexGuiScoreBackground");
 		_font = Resources.Load <Font> ("GUI/Fonts/FntDigitalNumbers");
-
-
+		
+		
 		//Texture assigned?
 		if (!_texMenuBackground) 
 		{
@@ -69,21 +70,21 @@ public class GuiReplay : MonoBehaviour {
 			Debug.LogError("Can't find Texture 'TexGuiButtonHover' on path 'Resources/GUI/Textures' ");
 			return;
 		}
-
+		
 		//Texture assigned?
 		if (!_texMenuTextfield) 
 		{
 			Debug.LogError("Can't find Texture 'TexGuiTextfield' on path 'Resources/GUI/Textures' ");
 			return;
 		}
-
+		
 		//Texture assigned?
 		if (!_texMenuBackground2) 
 		{
 			Debug.LogError("Can't find Texture 'TexGuiScoreBackground' on path 'Resources/GUI/Textures' ");
 			return;
 		}
-
+		
 		//Font assigned?
 		if (!_font) 
 		{
@@ -91,7 +92,7 @@ public class GuiReplay : MonoBehaviour {
 			return;
 		}
 	}
-
+	
 	/// <summary>
 	/// Raises the GUI event.
 	/// </summary>
@@ -100,20 +101,27 @@ public class GuiReplay : MonoBehaviour {
 		if(_showMenuReplay)
 		{
 			SetGuiSkin ();
-			Rect menuContent = DrawBackground (0.3f);
+			Rect menuContent = DrawBackground (0.3f, false);
 			DrawMenuButtons (menuContent,0.8f,300.0f, 20.0f);
+			
+			if(this.Enable3D)
+			{
+				Rect menuContent3D = DrawBackground (0.3f, this.Enable3D);
+				DrawMenuButtons (menuContent3D,0.8f,300.0f, 20.0f);
+			}
+			
 			AnimateMenuOnScreen (true);
 		}
 	}
 	#endregion
-
+	
 	#region Private Methods
 	/// <summary>
 	/// Sets the GUI skin.
 	/// </summary>
 	private void SetGuiSkin ()
 	{
-	
+		
 		//Font and Font Size for Label
 		GUI.skin.font = _font;
 		GUI.skin.label.fontSize = 50;
@@ -134,10 +142,10 @@ public class GuiReplay : MonoBehaviour {
 		GUI.skin.textField.active.background = _texMenuTextfield;
 		GUI.skin.textField.hover.background = _texMenuTextfield;
 		GUI.skin.textField.focused.background = _texMenuTextfield;
-
+		
 		GUI.skin.textField.focused.textColor = new Color (0.129f, 0.321f, 0.384f);
 	}
-
+	
 	/// <summary>
 	/// Gets the aspect ratio H.
 	/// </summary>
@@ -147,7 +155,7 @@ public class GuiReplay : MonoBehaviour {
 	{
 		return ((float)aTexture.height  / (float)aTexture.width );
 	}
-
+	
 	/// <summary>
 	/// Gets the aspect ratio W.
 	/// </summary>
@@ -157,70 +165,75 @@ public class GuiReplay : MonoBehaviour {
 	{
 		return ((float)aTexture.width / (float)aTexture.height);
 	}
-
+	
 	
 	/// <summary>
 	/// Draws the background.
 	/// </summary>
 	/// <returns>The background.</returns>
 	/// <param name="faktWidth">Fakt width.</param>
-	private Rect DrawBackground(float faktWidth){
-
+	private Rect DrawBackground(float faktWidth, bool enabled3D){
+		
 		//Max Breite nach Prozent von dem Bilschirm
 		float backgrW = Screen.width * faktWidth;
-
+		
 		//AspektRatio von Hintergrund
 		float backgrAspRatio =  GetAspectRatioHW (_texMenuBackground);
-
+		
 		//Die Höhe errechnen anhand der Breite
 		float backgrH = backgrW * backgrAspRatio;
-
+		
 		//Prüfen ob Höhe zu groß für den Bildschirm ist
 		if (backgrH > Screen.height) {
-		
+			
 			//Dann Höhe auf Max Bildschirm Höhe setzen
 			backgrH = Screen.height;
-
+			
 			//AspektRatio neu Berechnen um Breite zu bestimmen
 			backgrAspRatio = GetAspectRatioWH (_texMenuBackground);
-
+			
 			//Breite des Hintergrundes neu berechnen
 			backgrW = backgrH * backgrAspRatio;
-
+			
 		}
-
+		
 		//Oberen und Unteren Abstand berechnen
 		float backgrFreeVSpace = (Screen.height - backgrH) / 2;
-
-		GUI.DrawTexture(new Rect(0.0f - backgrW + _positionGuiX,backgrFreeVSpace, backgrW,backgrH), _texMenuBackground, ScaleMode.ScaleToFit, true, 0.0f);
-	
+		
+		if (!enabled3D)
+			GUI.DrawTexture (new Rect (0.0f - backgrW + _positionGuiX, backgrFreeVSpace, backgrW, backgrH), _texMenuBackground, ScaleMode.ScaleToFit, true, 0.0f);
+		else 
+			GUI.DrawTexture (new Rect (Screen.width * 0.5f - backgrW + _positionGuiX, backgrFreeVSpace, backgrW, backgrH), _texMenuBackground, ScaleMode.ScaleToFit, true, 0.0f);
+		
 		//Breite des Menus speichern
 		_menuWidth = backgrW;
-
+		
 		//Bereich/Platz für Inhalt berechnen
 		Rect contentRect = new Rect ();
-
+		
 		//Freier Platz + den Rahmen
 		contentRect.y = (backgrH * 0.05f) + backgrFreeVSpace;
-
+		
 		//Linker Rand des Bildschirms
-		contentRect.x = 0.0f - backgrW+ _positionGuiX;
-
+		if(!enabled3D)
+			contentRect.x = 0.0f - backgrW+ _positionGuiX;
+		else
+			contentRect.x = Screen.width * 0.5f - backgrW+ _positionGuiX;
 		//Die Breite des Verfügbaren Inhalts ohne Rand
 		contentRect.width = backgrW * 0.95f;
-
+		
 		//Die Höhe des Verfügbaren Inhalts ohne Rand
 		contentRect.height = backgrH * 0.9f;
-
+		
 		//Texture auf Repeat anstelle von Verzerren stellen
 		_texMenuBackground2.wrapMode = TextureWrapMode.Repeat;
 		
 		//Textur in Content Bereich malen in kleinen Blöcken der Textur
-		GUI.DrawTextureWithTexCoords(contentRect, _texMenuBackground2, new Rect(0, 0, contentRect.width / (_texMenuBackground2.width/2.5f), contentRect.height / (_texMenuBackground2.height/2.5f)));
-
+		GUI.DrawTextureWithTexCoords (contentRect, _texMenuBackground2, new Rect (0, 0, contentRect.width / (_texMenuBackground2.width / 2.5f), contentRect.height / (_texMenuBackground2.height / 2.5f)));
+		
 		return contentRect;
 	}
-
+	
 	/// <summary>
 	/// Draws the menu buttons.
 	/// </summary>
@@ -230,64 +243,63 @@ public class GuiReplay : MonoBehaviour {
 	/// <param name="minSpace">Minimum space.</param>
 	private void DrawMenuButtons (Rect content, float faktWidth, float minWidth, float minSpace) 
 	{
-	
+		
 		//Breite nach Prozent von dem Content
 		float buttonW = content.width * faktWidth;
-
+		
 		//Volle Breite von Content wenn kleiner als min
 		if(buttonW < minWidth)
 			buttonW = content.width;
 		
-
 		//AspektRatio von Button
 		float buttonAspRatio = GetAspectRatioHW (_texMenuButton);
-
+		
 		//Die Höhe errechnen anhand der Breite
 		float buttonH = buttonW * buttonAspRatio;
-
+		
 		//Prüfen ob alle 3 Buttons mit Platz dazwischen in Content passen
 		if (((buttonH * 3) + (minSpace * 4)) > content.height) 
 		{
 			//Höhe auf Resthöhe nach Abzug des MindestAbstandes
 			buttonH = (content.height - (4 * minSpace)) / 3;
-
+			
 			//AspektRatio neu Berechnen um Breite zu bestimmen
 			buttonAspRatio = GetAspectRatioWH (_texMenuButton);
 			
 			//Breite des Button neu berechnen
 			buttonW = buttonH * buttonAspRatio;
 		}
-
+		
 		//Zwischen Abstand der Buttons berechnen
 		float buttonFreeVSpace = (content.height - (buttonH * 3)) / 4;
 		float buttonFreeHSpace = (content.width - buttonW) / 2;
-
+		
 		GUI.skin.textField.padding.left = (int)(buttonW * 0.2f);
 		GUI.skin.textField.padding.right = (int)(buttonW * 0.2f);
 		GUI.skin.textField.padding.top = (int)(buttonH * 0.1f);
 		GUI.skin.textField.padding.bottom = (int)(buttonH * 0.1f);
-
+		
 		//Stimmt die Schriftgröße
 		int fontSize = (int)( buttonH * 0.3f) ;
-
+		
 		if(fontSize > 0)
 			GUI.skin.button.fontSize = fontSize;
-
+		
 		if (GUI.Button (new Rect (content.x + buttonFreeHSpace, content.y + (buttonFreeVSpace * 2) + (buttonH * 1), buttonW, buttonH), "Replay")) 
 		{
 			SaveNewHighscore();
 			Application.LoadLevel(_gameScene);
 		}
-
+		
 		if (GUI.Button (new Rect (content.x + buttonFreeHSpace, content.y + (buttonFreeVSpace * 3) + (buttonH * 2), buttonW, buttonH), "Menu"))
 		{
 			SaveNewHighscore();
 			Application.LoadLevel(_menuScene);
 		}
-
+		
 		GUI.skin.label.fontSize =  (int)(fontSize * 0.8f);
 		GUI.skin.textField.fontSize = (int)(fontSize * 0.8f);
-
+		
 		if(_isNewHighscore)
 		{
 			GUI.Label (new Rect (content.x + buttonFreeHSpace, content.y + (buttonFreeVSpace * 1) + (buttonH * 0), buttonW, buttonH / 2), "Neuer Highscore");
@@ -297,23 +309,23 @@ public class GuiReplay : MonoBehaviour {
 			GUI.TextField (new Rect (content.x + buttonFreeHSpace, content.y + (buttonFreeVSpace * 1) + (buttonH * 0.5f), buttonW, buttonH/2), "", 8);
 		}
 	}
-
+	
 	/// <summary>
 	/// Saves the new highscore.
 	/// </summary>
 	private void SaveNewHighscore()
 	{
 		if (_isNewHighscore) {
-
+			
 			if(_playerName.Length <= 0)
 				_playerName = "Player";
-
+			
 			GuiHighscoreData.AddScoreItem(_playerName, GuiHighscoreData._actPoints);
 			GuiHighscoreData._actPoints = 0;
 		} 
 	}
 	#endregion
-
+	
 	#region Public Methods
 	/// <summary>
 	/// Starts the menu replay.
@@ -323,7 +335,7 @@ public class GuiReplay : MonoBehaviour {
 		_isNewHighscore = GuiHighscoreData.IsNewHighscore (GuiHighscoreData._actPoints);
 		_showMenuReplay = true;
 	}
-
+	
 	/// <summary>
 	/// Animates the menu on screen.
 	/// </summary>
@@ -334,14 +346,14 @@ public class GuiReplay : MonoBehaviour {
 		{ //In den Screen bewegen
 			if(_positionGuiX < _menuWidth) 
 				_positionGuiX += _speed * Time.deltaTime;
-
+			
 			if(_positionGuiX >= _menuWidth)
 				_positionGuiX = _menuWidth;
 		}else{ //Aus dem Screen raus bewegen
 			
 			if(_positionGuiX > 0.0f) 
 				_positionGuiX -= _speed * Time.deltaTime;
-
+			
 			if(_positionGuiX <= 0.0f)
 				_positionGuiX = 0.0f;
 		}
@@ -349,4 +361,3 @@ public class GuiReplay : MonoBehaviour {
 	#endregion
 }
 
-	
